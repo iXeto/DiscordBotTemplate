09dbaa96d95cd8adcf160e0c9a53f4cb1faadbfc
iXeto <158444658+iXeto@users.noreply.github.com>
2024-06-13 12:12:52 +0200

    Add files via upload

---
+++ DiscordBotTemplate1/src/main/java/com/iXeto/Main.java
@@ -0,0 +1,14 @@
+package com.iXeto;
+
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+
+import javax.security.auth.login.LoginException;
+
+public class Main {
+    public static void main(String[] args) throws LoginException {
+        JDA jda = JDABuilder.createDefault("Paste Token Here").build();
+
+
+    }
+}
